b84b5cbb61922ccfbbf5db6fc63626b43c3fa248
Nishita Kapadia <nishita_mscit@yahoo.com>
2011-07-21 13:14:27 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VenuFinder::Application.config.secret_token = 'f67a4f9c338bf7aced619fd63cad40da47c2a378b86c3b39ebd7d3d69b9148c7c99619b579cc5da20b59fe7e5498d38b0fb36c52ce3d2e166ee2a81e828e2238'
